1972866a62ad9b9d0fa4476206df79eb1b38a580
bigpit <30524280+bigpit@users.noreply.github.com>
2020-03-10 22:47:54 +0200

    Финиш шапки с меню

--- js/main.js
+++ js/main.js
@@ -38,3 +38,16 @@ function adaptive_function() {
 	adaptive_header(w,h);
 }
 	adaptive_function();
+
+//Нажимание на бургег
+	$('.header-menu__icon').click(function(event) {
+		$(this).toggleClass('active');
+		$('.header-menu').toggleClass('active');
+		if($(this).hasClass('active')){
+			$('body').data('scroll',$(window).scrollTop());
+		}
+			$('body').toggleClass('lock');
+		if(!$(this).hasClass('active')){
+			$('body,html').scrollTop(parseInt($('body').data('scroll')));
+		}
+	});
